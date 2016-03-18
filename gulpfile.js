c944a2683731335a99734b8d5a6d97ba86e12075
Nidhi <nidhi@dottech.local>
2016-03-18 18:11:27 +0530

    Added coffee scripts files

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,18 @@
 var gulp = require('gulp'),
-gutil = require('gulp-util');
+gutil = require('gulp-util'),
+coffee = require('gulp-coffee');
 
-gulp.task('log', function() {
-  gutil.log('Workflows are awesome');
+//gulp.task('log', function() {
+  //gutil.log('Workflows are awesome');
 
+//for more coffee scripts we use variables
+//var coffeeSources = ['components/coffee/tagline or *.coffee']
+
+gulp.task('coffee', function() {
+   gulp.src('components/coffee/tagline.coffee')
+   //gulp.src(coffeeSources)
+     .pipe(coffee({ bare: true })
+        .on('error', gutil.log))
+        .pipe(gulp.dest('components/scripts'))
+     
 });
